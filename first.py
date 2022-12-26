e34995380231c493271a2020dac507506fa9c76a
AlifanovIlya <87135342+AlifanovIlya@users.noreply.github.com>
2022-12-26 17:56:23 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,3 +1,5 @@
+import characters
+
 def nextMessage():
 	input_key = input('\n***Что бы увидеть следующее сообщение, нажми клавишу "Enter"***\n')
 
@@ -5,140 +7,12 @@ def nextMessage():
 		print('\n*** Это какая-то другая клавиша! ***\n')
 		input_key = input('\n*** Что бы увидеть следующее сообщение, нажми клавишу "Enter" ***\n')
 
-def choseCharacter():
-	def characterRita():
-		name = 'Рита Янг'
-		role = 'Спортсменка'
-		health = 9
-		force = 5
-		agility = 4
-		attention = 3
-		knowledge = 3
-		communication = 2
-		will = 4
-		print(f'Рита Янг\n Спортсменка\n Здоровье = {health}\n Сила = {force}\n Ловкость = {agility}\n Внимание = {attention}\n Знания = {knowledge}\n Общение = {communication}\n Воля = {will}')
-
-	def characterKarson():
-		name = 'Карсон Синклер'
-		role = 'Дворецкий'
-		health = 8
-		force = 3
-		agility = 2
-		attention = 5
-		knowledge = 4
-		communication = 4
-		will = 3
-		print(f'Карсон Синклер\n Дворецкий\n Здоровье = {health}\n Сила = {force}\n Ловкость = {agility}\n Внимание = {attention}\n Знания = {knowledge}\n Общение = {communication}\n Воля = {will}')
-
-	def characterAgata():
-		name = 'Агата Крейн'
-		role = 'Парапсихолог'
-		health = 5
-		force = 2
-		agility = 3
-		attention = 4
-		knowledge = 5
-		communication = 3
-		will = 4
-		print(f'Агата Крейн\n Парапсихолог\n Здоровье = {health}\n Сила = {force}\n Ловкость = {agility}\n Внимание = {attention}\n Знания = {knowledge}\n Общение = {communication}\n Воля = {will}')
-
-	def characterVendi():
-		name = 'Венди Адамс'
-		role = 'Беспризорница'
-		health = 6
-		force = 3
-		agility = 5
-		attention = 4
-		knowledge = 3
-		communication = 3
-		will = 3
-		print(f'Венди Адамс\n Беспризорница\n Здоровье = {health}\n Сила = {force}\n Ловкость = {agility}\n Внимание = {attention}\n Знания = {knowledge}\n Общение = {communication}\n Воля = {will}')
-
-	def characterPreston():
-		name = 'Престон Фэрмонт'
-		role = 'Миллионер'
-		health = 8
-		force = 4
-		agility = 4
-		attention = 3
-		knowledge = 2
-		communication = 5
-		will = 3
-		print(f'Пестон Фэрмонт\n Миллионер\n Здоровье = {health}\n Сила = {force}\n Ловкость = {agility}\n Внимание = {attention}\n Знания = {knowledge}\n Общение = {communication}\n Воля = {will}')
-
-	def characterMin():
-		name = 'Минь Тхи Фанг'
-		role = 'Серкетарша'
-		health = 7
-		force = 3
-		agility = 4
-		attention = 4
-		knowledge = 3
-		communication = 4
-		will = 3
-		print(f'Минь Тхи Фанг\n Серкетарша\n Здоровье = {health}\n Сила = {force}\n Ловкость = {agility}\n Внимание = {attention}\n Знания = {knowledge}\n Общение = {communication}\n Воля = {will}')
-
-	def characterUilyam():
-		name = 'Уильям Йорик'
-		role = 'Могильщик'
-		health = 7
-		force = 4
-		agility = 3
-		attention = 4
-		knowledge = 3
-		communication = 3
-		will = 4
-		print(f'Уильям Йорик\n Могильщик\n Здоровье = {health}\n Сила = {force}\n Ловкость = {agility}\n Внимание = {attention}\n Знания = {knowledge}\n Общение = {communication}\n Воля = {will}')
-
-	def characterMateo():
-		name = 'Отец Матео'
-		role = 'Священник'
-		health = 6
-		force = 3
-		agility = 3
-		attention = 2
-		knowledge = 4
-		communication = 4
-		will = 5
-		print(f'Отец Матео\n Священник\n Здоровье = {health}\n Сила = {force}\n Ловкость = {agility}\n Внимание = {attention}\n Знания = {knowledge}\n Общение = {communication}\n Воля = {will}')
-
-	characterRita()
-
-	nextMessage()
-
-	characterKarson()
-
-	nextMessage()
-
-	characterAgata()
-
-	nextMessage()
-
-	characterVendi()
-
-	nextMessage()
-
-	characterPreston()
-
-	nextMessage()
-
-	characterMin()
-
-	nextMessage()
-
-	characterUilyam()
-
-	nextMessage()
-
-	characterMateo()
-
-
-
-
-
 
 def main():
-	print('Привет, сыщик! Ты в особняке безумия, и тебе необходимо, ты не поверишь, найти! А вот что найти, это тебе предстоит узнать.')
+	print('''Ну, здравствуй!
+Добро пожаловать в дом ужасов Ха-Ха!
+Ты - сыщик, и тебе надо, кто бы мог подумать, найти!
+А вот, что найти, это ты и узнаешь!''')
 
 	nextMessage()
 
@@ -146,13 +20,26 @@ def main():
 
 	nextMessage()
 
-	choseCharacter()
+	characters.lookAtCharacters()
 
 	nextMessage()
 
-	mainCharacter = input('Впиши полное имя персонажа, которого хочешь выбрать')
+	mainCharacterName = input('Впиши полное имя персонажа, которого хочешь выбрать\n\n')
+	characterNamesArray = ['Рита Янг',
+	'Карсон Синклер',
+	'Агата Крейн',
+	'Венди Адамс',
+	'Престон Фэрмонт',
+	'Минь Тхи Фанг',
+	'Уильям Йорик',
+	'Отец Матео']
+	while mainCharacterName not in characterNamesArray:
+		mainCharacterName = input('Это не правильное имя, впиши полное имя персонажа, которого хочешь выбрать\n\n')
+
+	print('\nОтлично, а теперь продолжим.\n')
+
+	print(characters.charactersSpecials(mainCharacterName, 'health'))
 
 
-
-
-main()
+if __name__ == '__main__':
+	main()
