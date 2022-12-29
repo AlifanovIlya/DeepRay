88c248c67201a491cd28e992dd430a8eb441f40b
AlifanovIlya <87135342+AlifanovIlya@users.noreply.github.com>
2022-12-29 18:23:41 +0300

    Add files via upload

---
+++ rockScissorsPaper.py
@@ -0,0 +1,37 @@
+from random import randint
+
+def game():
+	computerChoose = randint(1, 1)
+
+	gamerChoose = input('''Что бы пройти дальше,тебе нужно победить в игре "Камень, ножницы, бумага".\n
+Впиши то, что ты хочешь выбрать.\n\n''')
+
+	if gamerChoose == 'камень':
+		if computerChoose == 1:
+			print('\nУ соперника тоже выпал Камень, нужно сыграть снова.')
+		elif computerChoose == 2:
+			print('\nУ соперника выпали Ножницы, ты победил!')
+			return True
+		elif computerChoose == 3:
+			print('\nУ соперника выпала Бумага, ты проиграл. Очень жаль.')
+			return False
+	elif gamerChoose == 'ножницы':
+		if computerChoose == 1:
+			print('\nУ соперника выпал Камень, ты проиграл. Очень жаль')
+			return False
+		elif computerChoose == 2:
+			print('\nУ соперника тоже выпали Ножницы, нужно сыграть снова')
+		elif computerChoose == 3:
+			print('\nУ соперника выпала Бумага, ты победил!')
+			return True
+	elif gamerChoose == 'бумага':
+		if computerChoose == 1:
+			print('\nУ соперника выпал Камень, ты победил!')
+			return True
+		elif computerChoose == 2:
+			print('\nУ соперника выпали Ножницы, ты проиграл. Очень жаль')
+			return False
+		elif computerChoose == 3:
+			print('\nУ соперника тоже выпала Бумага, нужно сыграть снова.')
+	else:
+		print('\nТы вписал что-то не то. Впиши, то, что ты хочешь выбрать.')
